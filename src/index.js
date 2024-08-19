22e86c9e562a39d836cb88bf6ce9a2ac16f0ce03
Leachenski <jcleachenski@gmail.com>
2024-08-19 19:41:17 -0300

    init: primeiro commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.get("/naipe", (req, res) => {
+    
+})
+
+app.listen(port, () => {
+    console.log("isso aqui executa quando o servidor inicia")
+})
